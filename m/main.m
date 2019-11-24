1af437a8306c6071b4d77f9fca8349d823e1f296
silvacesar <csilva@fandm.edu>
2019-11-24 12:50:53 -0500

    Can plot digits

--- m/main.m
+++ m/main.m
@@ -1,1 +1,7 @@
 meanStruct= getMnistMeans('../datasets/mnist_all.mat');
+for i= 0:9
+    fieldname= strcat('trainMean', int2str(i));
+    digit=meanStruct.(fieldname);
+    figure;
+    plotDigit(digit);
+end
