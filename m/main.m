fc4d66fe94b3bf573d9b9e45fbdfbb9453f92fc8
gregbolet <gbolet@fandm.edu>
2019-12-08 12:43:20 -0500

    Got HW/FG accuracies

--- m/main.m
+++ m/main.m
@@ -12,16 +12,24 @@ load('../datasets/fontGenTestData.mat');
 avrgsMnist   = avrgInputDigits(mnistInputTrain,   mnistTargetTrain);
 avrgsFontGen = avrgInputDigits(fontGenInputTrain, fontGenTargetTrain);
 
-%res = avrgClassifier(mnistInputTest, avrgsMnist);
-%resConf = figure, plotconfusion(mnistTargetTest, res, 'Testing MNIST');
-%set(findobj(resConf,'type','text'),'fontsize',8)
-
-res = avrgClassifier(fontGenInputTest, avrgsFontGen);
-resConf = figure, plotconfusion(fontGenTargetTest, res, 'Testing FONTGEN');
+res = avrgClassifier(mnistInputTest, avrgsMnist);
+resConf = figure, plotconfusion(mnistTargetTest, res, 'MNIST -- MNIST');
 set(findobj(resConf,'type','text'),'fontsize',8)
 
-plotDigit(reshape(mnistInputTest(:,2500),28,28));
-translateDigit(mnistInputTest(:,2500),7);
+res = avrgClassifier(fontGenInputTest, avrgsFontGen);
+resConf = figure, plotconfusion(fontGenTargetTest, res, 'FONTGEN -- FONTGEN');
+set(findobj(resConf,'type','text'),'fontsize',8)
+
+res = avrgClassifier(mnistInputTest, avrgsFontGen);
+resConf = figure, plotconfusion(mnistTargetTest, res, 'FONTGEN -- MNIST');
+set(findobj(resConf,'type','text'),'fontsize',8)
+
+res = avrgClassifier(fontGenInputTest, avrgsMnist);
+resConf = figure, plotconfusion(fontGenTargetTest, res, 'MNIST -- FONTGEN');
+set(findobj(resConf,'type','text'),'fontsize',8)
+
+%plotDigit(reshape(mnistInputTest(:,2500),28,28));
+%translateDigit(mnistInputTest(:,2500),7);
 
 %plotDigit(reshape(mnistInputTest(:,4),28,28));
 
